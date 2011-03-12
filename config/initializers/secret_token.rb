b0e19ee2873a79e5d2f2d09b3822376175391806
Benson Kalahar <bensonk42@gmail.com>
2011-03-11 23:41:11 -0600

    Initial project creation

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bensonk::Application.config.secret_token = '468d04c229b9307e683780a3d15a6316f24fc8b7e9c4b01cc49e06650b98e33c9b28034d0c1079b37f21ed865a3f5de82dfdb45ef6f6323a85e25c379e281aa8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Bensonk::Application.config.session_store :cookie_store, :key => '_bensonk_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Bensonk::Application.config.session_store :active_record_store
