084e781245437565fc08abec6b5957007331be4d
bensonk <bensonk42@gmail.com>
2012-04-15 22:26:17 -0700

    More 3.2 updates

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Bensonk::Application.config.secret_token = '468d04c229b9307e683780a3d15a6316f24fc8b7e9c4b01cc49e06650b98e33c9b28034d0c1079b37f21ed865a3f5de82dfdb45ef6f6323a85e25c379e281aa8'
+Bensonk::Application.config.secret_token = '4cd3a5f560204edd446d0361e345cda24f11c67eb0b35be96fc75ede1bbe1ed15bd660009bcefcb8523134f56ff6887aac3a72a7d1355c4c0e3df799081c4368'
--- public/javascripts/prettify/auto_prettify.js
+++
@@ -1,4 +0,0 @@
-$(function() {
-  $("code").addClass("prettyprint");
-  prettyPrint();
-});
